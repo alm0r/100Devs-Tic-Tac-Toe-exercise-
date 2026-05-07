df1062483446c2c26278684d97e1ad97c0e5a4d7
alm0r <almor.dev@protonmail.com>
2026-05-07 19:48:06 +0200

    fixing start/reset logic backend

--- js/main.js
+++ js/main.js
@@ -4,14 +4,6 @@ console.log("main script working");
 // TO DO
 //////////////////
 
-// Gameloop
-// Switching between players
-// OOP requirements
-// Check winning condition (when a player gets the 3 cells).
-
-/*Added to keep track of the updated cells*/
-// let board = ["cell-0", "cell-1", "cell-2", "cell-3", "cell-4", "cell-5", "cell-6", "cell-7", "cell-8"];
-
 //Introduced to keep track of the turn but alsoto switch between players, I think it will also be helpful for declaring a stalemate since I am pretty sure after 9 turns if no one has won the game its supposed to start over.
 let turn = 0;
 //This is just to list the players and their symbols, this will be used to feed to the player object as arguments.
@@ -20,8 +12,7 @@ let players = [
   ["player2", "O"],
 ];
 
-// let playerWins = false;
-
+let gameBeingPlayed = false;
 ///////////////////////////////////////////////////////////////////////////////////////////////
 // Players are cllases following OOP design
 ///////////////////////////////////////////////////////////////////////////////////////////////
@@ -88,53 +79,51 @@ class Player {
 // On page load, this waits for a click in order to start the game
 document.getElementById("game-start-button").addEventListener("click", () => {
   console.log("Game starts");
+  gameBeingPlayed = true;
   gameInit();
 });
 
 function gameInit() {
+  // if (gameBeingPlayed === false) return;
   // Creates a new board when the start button is clicked,making it also work as a Reset bvutton
+  turn = 0;
   board = ["cell-0", "cell-1", "cell-2", "cell-3", "cell-4", "cell-5", "cell-6", "cell-7", "cell-8"];
   document.querySelectorAll(".game-cell").forEach((element) => {
     element.innerText = "";
   });
+
   // Creates new Cell / Makes a player Move
+
   document.querySelectorAll(".game-cell").forEach((element) => {
     element.addEventListener("click", () => {
       // Creates a new board variable
+      if (gameBeingPlayed === true) {
+        // checks if its working on the console, debugging purpose
+        console.log(`clicked on ${element.id}`);
+        // gets the id number
+        let idNumber = element.id[element.id.length - 1];
+        // creates player Object and picks the cell after clicking on it on the DOM. This right now is hardcoded because there is no switching between players states logic
+        const choosenPlayer = new Player(players[turn % players.length][0], players[turn % players.length][1]);
 
-      // checks if its working on the console, debugging purpose
-      console.log(`clicked on ${element.id}`);
-      // gets the id number
-      let idNumber = element.id[element.id.length - 1];
-      // creates player Object and picks the cell after clicking on it on the DOM. This right now is hardcoded because there is no switching between players states logic
-      const choosenPlayer = new Player(players[turn % players.length][0], players[turn % players.length][1]);
-
-      // Updates the chosen cell on the DOM and blocks the player turn untill he chooses a correct cell
-      if (document.getElementById(`cell-${idNumber}`).innerText === "") {
-        document.getElementById(`cell-${idNumber}`).innerText = choosenPlayer.symbol;
-        // Next turn
-        turn += 1;
-      } else {
-        console.log(`Try another cell, this one is already in use`);
-      }
-      // Cheks if the choosen cell makes matches the game winning condition
-      choosenPlayer.createCell(true, idNumber);
-      if (choosenPlayer.checkWinCon() === true) {
-        console.log(`Congrats ${choosenPlayer.player}, you win`);
-        document.querySelector(".txt-msg").innerText = `Congrats ${choosenPlayer.player}, you win!`;
+        // Updates the chosen cell on the DOM and blocks the player turn untill he chooses a correct cell
+        if (document.getElementById(`cell-${idNumber}`).innerText === "") {
+          document.getElementById(`cell-${idNumber}`).innerText = choosenPlayer.symbol;
+          // Next turn
+          turn += 1;
+        } else {
+          console.log(`Try another cell, this one is already in use`);
+        }
+        // Cheks if the choosen cell makes matches the game winning condition
+        choosenPlayer.createCell(true, idNumber);
+        if (choosenPlayer.checkWinCon() === true) {
+          gameBeingPlayed = false;
+          console.log(`Congrats ${choosenPlayer.player}, you win`);
+          document.querySelector(".txt-msg").innerText = `Congrats ${choosenPlayer.player}, you win!`;
+        }
       }
     });
   });
 }
-
-//Resetting both the board and the cells for now, I think we could defniteley make it so the cell just follow what's ont he board to centralize it though. WOrking on it.
-
-// function resetGame() {
-//   board = ["cell-0", "cell-1", "cell-2", "cell-3", "cell-4", "cell-5", "cell-6", "cell-7", "cell-8"];
-//   document.querySelectorAll(".game-cell").forEach((element) => {
-//     element.innerText = "";
-//   });
-// }
 
 //////////////////////////////////////////////////////////////////////////////////////////
 // This right now does nothing usefull to the actual project.
@@ -149,80 +138,3 @@ document.querySelectorAll(".game-cell").forEach((element) => {
 });
 
 ///////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////////
-
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
-
-// FOR POTENTIAL FUTURE REFACTORING DO NOT DELETE
-
-///////////////////////////////////////////////////
-// SWITCHING BETWEEN PLAYERS LOGIC PROTOTYPE
-/////////////////////////////////////////////////
-
-// This let playerWins = false; for now works as a global variable to determine the outcome of thw switch statement inside checkWinCon()
-// but is unnecesary (and kind of reduntand) and could be replaced byt simply using a return true inside the switch
-// I think its use depends on which kind logic we end up using for the switching between players, but its is posible to make checkWinCon() to simply return
-// a boolean (as i tested in the first case with return true) and then simply check if player1.checWinCon() is returned true or false instead of checking the playerWinds variable
-// i will leave for now because its the first iteration and i think is easy and simpler to understand
-
-// This works by using the global variable let gameBeingPlayed = true; in a while loop. The while loop switches between 2 states determined byt 2 other global variables
-// let playerOneTurn = true; and // let playerTwoTurn = false; and alternates between them. While one of the if statements is true, it runs, and at then end, it becames false and changes
-// the toogles the other variable ( basically working like a true/false switch). The while (gameBeingPlayed === true) is an defacto infinity loop and runs as long as its condition is true but the idea is
-// it should be very easy to exit with a simple if statement like: if the winning condition for a player is true => gameBeingPlayed === false === break out of the loop
-
-// let playerOneTurn = true;
-// let playerTwoTurn = false;
-
-// let gameBeingPlayed = true;
-
-// // initTurn is just a temporary placeholder for the createCell() to see if the logic of the while loop + if statements works (it does)
-// function initTurn(player) {
-//   console.log(`${player} makes his move`);
-// }
-
-// function initGame() {
-//   while (gameBeingPlayed === true) {
-//     if (playerOneTurn === true) {
-//       // this is just for debuging to see the if its working properly
-//       console.log("while loop working for player-1");
-
-//       initTurn("player-1");
-//       // this is what make the while loop swtich between both player "infinitely".
-//       playerOneTurn = false;
-//       playerTwoTurn = true;
-//     } else if (playerTwoTurn === true) {
-//       // this is just for debuging to see the if its working properly
-//       console.log("while loop working for player-2");
-//       initTurn("player-2");
-//       playerOneTurn = true;
-//       playerTwoTurn = false;
-//       // break;
-//       // Once we add the logic for determining if a player move its a winning move it should be pretty easy to add an gameBeingPlayed = false to exit the while loop,
-//       //  for now this is acting as a break because otherwise the loops infinetly
-//       gameBeingPlayed = false;
-//     }
-//   }
-// }
-
-// initGame();
